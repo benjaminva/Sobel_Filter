67d09bbf27feb030add6ca3ad1c3d0136408fe4e
unknown <analup250@gmail.com>
2021-04-13 12:18:54 -0500

    agregar detección de bordes

--- sobel.py
+++ sobel.py
@@ -2,6 +2,39 @@ import numpy as np
 import cv2
 import argparse
 from convolution import convolution
+from gaussian_blur import gaussian_blur
+import matplotlib.pyplot as plt
+
+def sobel_edge_detection(image, filter, verbose=False):
+    new_image_x = convolution(image, filter, verbose)
+
+    if verbose:
+        plt.imshow(new_image_x, cmap='gray')
+        plt.title("Horizontal Edge")
+        plt.show()
+
+    new_image_y = convolution(image, np.flip(filter.T, axis=0), verbose)
+
+    if verbose:
+        plt.imshow(new_image_y, cmap='gray')
+        plt.title("Vertical Edge")
+        plt.show()
+
+    gradient_magnitude = np.sqrt(np.square(new_image_x) + np.square(new_image_y))
+
+    if verbose:
+        plt.imshow(gradient_magnitude, cmap='gray')
+        plt.title("Gradient Magnitude")
+        plt.show()
+
+    gradient_magnitude *= 255.0 / gradient_magnitude.max()
+
+    if verbose:
+        plt.imshow(gradient_magnitude, cmap='gray')
+        plt.title("Gradient Magnitude")
+        plt.show()
+
+    return gradient_magnitude
 
 if __name__ == '__main__':
     #sobel filter mask/kernel 
@@ -13,5 +46,6 @@ if __name__ == '__main__':
     #use open cv 2 to change the image into an array of numbers 
     image = cv2.imread(args["image"]) 
 
-    # To test the first part of convolution function
-    convolution(image, filter, verbose=True) 
+    #blur the image using a gaussian filter
+    image = gaussian_blur(image, 9, verbose=True)
+    sobel_edge_detection(image, filter, verbose=True) 
